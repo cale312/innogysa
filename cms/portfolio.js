50f818c5c3879e2d5366351092efd97140f9653d
Zikisa Cale <zikisacalesogaxa@gmail.com>
2018-05-17 11:29:49 +0200

    added cms on the ortfolio page

---
+++ cms/portfolio.js
@@ -0,0 +1,10 @@
+({
+	heading: {
+		header: 'OUR PORTFOLIO'
+    },
+    description: {
+        sub_heading: 'Striving for perfection.',
+        paragraph_1: 'South Africa might be home to the big 5 game animals, but when it comes to design projects, we have more than that.',
+        paragraph_2: 'Duo utinam option ex, etiam luptatum consequat id qui, cum alii dicat nostro id! Ne nam feugait persecuti voluptatum, latine singulis expetendis est an? Sit eros qualisque liberavisse ea! Eu impetus facilisi mandamus ius.'
+    }
+})
