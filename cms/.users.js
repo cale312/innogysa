2a3290921cd79cf461b23bb10adb1ed7a394bf27
Zikisa Cale <zikisacalesogaxa@gmail.com>
2018-05-17 11:08:25 +0200

    initial commit

---
+++ cms/.users.js
@@ -0,0 +1,11 @@
+({
+	users: [
+		{
+			username: 'cale',
+			tags: [],
+			salt: '8cef49ee9422865980fb9ae281dca96c',
+			hash: 'b9d03eba9e208ef6b1b1bbe03f617b6689a62420195b16a2d4d97ea41c44e43a',
+			user_created_timestamp: 1526544727799
+		}
+	]
+})
---
+++ cms/index.js
@@ -0,0 +1,13 @@
+({
+	first_slide_left: {
+		header: 'We are Innogy\'s Digital Creative Agency'
+	},
+	first_slide_right: {
+		sub_heading1: 'Who we are',
+		about1: 'The Garage was launched in 2017 as part of innogy SE, a leading European energy company. Local Garages were created to accelerate digital product design and development for innogy.',
+		sub_heading2: 'Our goal',
+		about2: 'We bring researchers, designers and technologists together to create the best digital solutions for your customers.',
+		sub_heading3: 'We are international',
+		about3: 'With Garages in Amsterdam, Berlin and Cape Town.'
+	}
+})
