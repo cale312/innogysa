a667e65b3abe40ef5cb72cd7602be8bf0ebfccef
Nonzulu <knzulu16@gmail.com>
2018-05-17 12:09:47 +0200

    nav changes

---
+++ cms/contact.js
@@ -0,0 +1,7 @@
+({
+    portfolio_description:{
+        heading:'Voice it out, as loud as a vuvuela!',
+        sub_heading :'Striving for perfection.',
+        about_portfolio:'South Africa might be home to the big 5 game animals, but when it comes to design projects, we have more than that.'
+    },
+})
