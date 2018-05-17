d647ed7240843d506939a3751810baba22cff70e
Zikisa Cale <zikisacalesogaxa@gmail.com>
2018-05-17 14:49:28 +0200

    beautify

--- cms/index.js
+++ cms/index.js
@@ -2,15 +2,16 @@
 	first_slide_left: {
 		header: 'We are Innogy\'s Digital Creative Agency'
 	},
-	first_slide_right: [{
+	first_slide_right: [
+		{
 			$paragraph_type: 'textarea',
 			sub_heading: 'Who we are',
-			paragraph: 'The Garage was launched in 2017 as part of innogy SE, a leading European energy company. Local Garages were created to accelerate digital product design and development for innogy.',
+			paragraph: 'The Garage was launched in 2017 as part of innogy SE, a leading European energy company. Local Garages were created to accelerate digital product design and development for innogy.'
 		},
 		{
 			$paragraph_type: 'textarea',
 			sub_heading: 'Our goal',
-			paragraph: 'We bring researchers, designers and technologists together to create the best digital solutions for your customers.',
+			paragraph: 'We bring researchers, designers and technologists together to create the best digital solutions for your customers.'
 		},
 		{
 			$paragraph_type: 'textarea',
