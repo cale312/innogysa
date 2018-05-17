034a46556552d30c69e4af94b7ac3c330c8a5fc1
Zikisa Cale <zikisacalesogaxa@gmail.com>
2018-05-17 14:39:00 +0200

    wip

--- cms/index.js
+++ cms/index.js
@@ -2,12 +2,20 @@
 	first_slide_left: {
 		header: 'We are Innogy\'s Digital Creative Agency'
 	},
-	first_slide_right: {
-		sub_heading1: 'Who we are',
-		about1: 'The Garage was launched in 2017 as part of innogy SE, a leading European energy company. Local Garages were created to accelerate digital product design and development for innogy.',
-		sub_heading2: 'Our goal',
-		about2: 'We bring researchers, designers and technologists together to create the best digital solutions for your customers.',
-		sub_heading3: 'We are international',
-		about3: 'With Garages in Amsterdam, Berlin and Cape Town.'
-	}
+	first_slide_right: [{
+			$paragraph_type: 'textarea',
+			sub_heading: 'Who we are',
+			paragraph: 'The Garage was launched in 2017 as part of innogy SE, a leading European energy company. Local Garages were created to accelerate digital product design and development for innogy.',
+		},
+		{
+			$paragraph_type: 'textarea',
+			sub_heading: 'Our goal',
+			paragraph: 'We bring researchers, designers and technologists together to create the best digital solutions for your customers.',
+		},
+		{
+			$paragraph_type: 'textarea',
+			sub_heading: 'We are international',
+			paragraph: 'With Garages in Amsterdam, Berlin and Cape Town.'
+		}
+	]
 })
--- cms/portfolio.js
+++ cms/portfolio.js
@@ -1,19 +1,45 @@
 ({
 	heading: {
 		header: 'OUR PORTFOLIO'
-    },
-    description: {
-        sub_heading: 'Striving for perfection.',
-        paragraph_1: 'South Africa might be home to the big 5 game animals, but when it comes to design projects, we have more than that.',
-        paragraph_2: 'Duo utinam option ex, etiam luptatum consequat id qui, cum alii dicat nostro id! Ne nam feugait persecuti voluptatum, latine singulis expetendis est an? Sit eros qualisque liberavisse ea! Eu impetus facilisi mandamus ius.'
-    },
-    projects: {
-        project1: '/assets/img/1.png',
-        project2: '/assets/img/2.png',
-        project3: '/assets/img/3.png',
-        project4: '/assets/img/4.png',
-        project5: '/assets/img/5.png',
-        project6: '/assets/img/6.png',
-
-    }
+	},
+	description: [
+		{
+			$sub_heading_type: 'textarea',
+			sub_heading: 'Striving for perfection.'
+		},
+		{
+			$paragraph_type: 'textarea',
+			paragraph: 'South Africa might be home to the big 5 game animals, but when it comes to design projects, we have more than that.'
+		},
+		{
+			$paragraph_type: 'textarea',
+			paragraph: 'Duo utinam option ex, etiam luptatum consequat id qui, cum alii dicat nostro id! Ne nam feugait persecuti voluptatum, latine singulis expetendis est an? Sit eros qualisque liberavisse ea! Eu impetus facilisi mandamus ius.'
+		}
+	],
+	projects: [
+		{
+			$image_type: 'image',
+			image: '/assets/img/1.png'
+		},
+		{
+			$image_type: 'image',
+			image: '/assets/img/2.png'
+		},
+		{
+			$image_type: 'image',
+			image: '/assets/img/3.png'
+		},
+		{
+			$image_type: 'image',
+			image: '/assets/img/4.png'
+		},
+		{
+			$image_type: 'image',
+			image: '/assets/img/5.png'
+		},
+		{
+			$image_type: 'image',
+			image: '/remote/direct_uploads/1526560307_6.png'
+		}
+	]
 })
