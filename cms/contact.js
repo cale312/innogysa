1778037967ab605340c9e145571944cc9b5ddcd4
Nonzulu <knzulu16@gmail.com>
2018-05-17 15:43:31 +0200

    worked on text area and paragraph alignment

--- cms/contact.js
+++ cms/contact.js
@@ -1,7 +1,14 @@
 ({
-    portfolio_description:{
-        heading:'Voice it out, as loud as a vuvuela!',
-        sub_heading :'Striving for perfection.',
-        about_portfolio:'South Africa might be home to the big 5 game animals, but when it comes to design projects, we have more than that.'
-    },
+	heading: 'Voice it out, as loud as a vuvuela!',
+	sub_heading: 'Striving for perfection.',
+	portfolio_description: [
+		{
+			$paragraph_type: 'textarea',
+			paragraph: 'South Africa might be home to the big 5 game animals, but when it comes to design projects, we have more than that.'
+		},
+		{
+			$paragraph_type: 'textarea',
+			paragraph: 'Duo utinam option ex, etiam luptatum consequat id qui, cum alii dicat nostro id! Ne nam feugait persecuti voluptatum, latine singulis expetendis est an? Sit eros qualisque liberavisse ea! Eu impetus facilisi mandamus ius.'
+		}
+	]
 })
