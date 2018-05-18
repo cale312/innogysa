e12cd28eb0d83c68132218ab01393a03db77e8ad
Zikisa Cale <zikisacalesogaxa@gmail.com>
2018-05-18 10:51:27 +0200

    added new admin user

--- cms/.users.js
+++ cms/.users.js
@@ -6,6 +6,13 @@
 			salt: '8cef49ee9422865980fb9ae281dca96c',
 			hash: 'b9d03eba9e208ef6b1b1bbe03f617b6689a62420195b16a2d4d97ea41c44e43a',
 			user_created_timestamp: 1526544727799
+		},
+		{
+			username: 'admin',
+			tags: [],
+			salt: 'e4baaa2aee8f1f912f718ff4d40c12b6',
+			hash: '1cb648156dbf36c09871d8f0e81b4890cbf399375961d25d400085ceed7e52aa',
+			user_created_timestamp: 1526633410677
 		}
 	]
 })
